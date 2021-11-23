48450df8d6ccb99c9fb71a76e4ab39cd1e920ebe
eveg <eveg@gost.local>
2021-11-23 14:34:37 +0200

    renamed:    .env -> .env_template 	modified:   .gitignore 	deleted:    CODE_OF_CONDUCT.md 	modified:   docker-compose.yml 	modified:   openvpn/docker-entrypoint.sh, do not use static server ip range

--- openvpn/docker-entrypoint.sh
+++ openvpn/docker-entrypoint.sh
@@ -6,9 +6,8 @@ if [ ! -f "/etc/openvpn/easy-rsa/pki/ca.crt" ]; then
   ./easyrsa build-ca nopass
   # Generate Diffie-Hellman parameters
   ./easyrsa gen-dh
-  # Genrate server keypair
+  # Generate server keypair
   ./easyrsa build-server-full server nopass
-
   # Generate shared-secret for TLS Authentication
   openvpn --genkey --secret pki/ta.key
   cp /etc/openvpn/easy-rsa/pki/{ca.crt,ta.key,issued/server.crt,private/server.key,dh.pem} "/etc/openvpn/"
@@ -38,8 +37,9 @@ iptables -I OUTPUT -o tun0 -j ACCEPT
 
 iptables -A FORWARD -i tun0 -o $primary_nic -j ACCEPT
 iptables -t nat -A POSTROUTING -o $primary_nic -j MASQUERADE
-iptables -t nat -A POSTROUTING -s 10.1.0.0/24 -o $primary_nic -j MASQUERADE
-iptables -t nat -A POSTROUTING -s 10.1.0.2/24 -o $primary_nic -j MASQUERADE
+iptables -t nat -A POSTROUTING -s "${SERVER_IP_RANGE}.0/24" -o $primary_nic -j MASQUERADE
+iptables -t nat -A POSTROUTING -s "${SERVER_IP_RANGE}.2/24" -o $primary_nic -j MASQUERADE
+
 
 # ensure that we are using the port specifiedby HOST_SSL_PORT
 sed -i "s/port 443/port $HOST_SSL_PORT/" /etc/openvpn/server.conf;
